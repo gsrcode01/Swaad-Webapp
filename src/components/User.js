04b81660ee6568704e8d09a9d20de6f0c9b10795
Girdhar Singh <girdharsinghrajpurohit353@gmail.com>
2026-07-17 23:26:31 +0530

    added user profile in the WebApp

---
+++ src/components/User.js
@@ -0,0 +1,13 @@
+const User = ({ name }) => {
+    return (
+        <>
+            <div>
+                <h2>Name:{name}</h2>
+                <h3>Location:Jodhpur</h3>
+                <h4>Contact:1223456789
+                </h4>
+            </div>
+        </>
+    );
+}
+export default User;
